79926bcea3e6327b9b394267b3a0b522fa24f946
sebobrien <sebobrien@gmail.com>
2018-07-25 13:52:35 +0200

    first commit

---
+++ movie-service/run.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+echo "********************************************************"
+echo "Starting movie-servicer"
+echo "********************************************************"
+java -jar /usr/local/movie-servie/movie-service-0.0.1-SNAPSHOT.jar
